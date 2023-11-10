481913cbc5aff0a0d1a2202979f763df60a84b1a
bsverdin <117467689+bsverdin@users.noreply.github.com>
2023-11-09 19:20:28 -0600

    Add files via upload

---
+++ Belinda/src/main/java/frc/robot/commands/AutoStartOnLeft.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class AutonomousDistance extends SequentialCommandGroup {
+  /**
+   * Creates a new Autonomous Drive based on distance. This will drive out for a specified distance,
+   * turn around and drive back.
+   *
+   * @param drivetrain The drivetrain subsystem on which this command will run
+   */
+  public AutonomousDistance(Drivetrain drivetrain) {
+    addCommands(
+        //units in inches
+        //grabs two left pieces and heads back to the base
+        new DriveDistance(-0.5, 26.4, drivetrain),
+        new TurnDegrees(-0.5, 90, drivetrain),
+        new DriveDistance(-0.5, 14.4, drivetrain),
+        new TurnDegrees(0.5, 90, drivetrain),
+        new DriveDistance(-0.5, 26.4, drivetrain));
+  }
+}
